2ac90bf329aee32de60fefede780d7912f9f9160
Ali Abdallah Massri <zismail7@me.com>
2025-07-15 01:13:50 +0300

    cc

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 const connectToMongoDB = async () => {
 	try {
-		await mongoose.connect("mongodb+srv://smeha273:RdBNbsweLI4FBGug@cluster0.xxkcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+		await mongoose.connect(process.env.MONGO_DB_URI);
 		console.log("Connected to MongoDB");
 	} catch (error) {
 		console.log("Error connecting to MongoDB", error.message);
